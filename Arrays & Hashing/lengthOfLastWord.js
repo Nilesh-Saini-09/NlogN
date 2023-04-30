d8d983fbd5e22b7ec5a3ca163bc193728a94502c
N <cubingniles@gmail.com>
2023-04-30 14:26:52 +0530

    Add more solutions

--- Arrays & Hashing/lengthOfLastWord.js
+++ Arrays & Hashing/lengthOfLastWord.js
@@ -5,5 +5,32 @@ const lengthLastWord = s => {
     return arr[arr.length - 1].length;
 }
 
-// 2. 
+// 2. While Loop
+// time - O(n), space - O(1)
+const lengthLastWord2 = s => {
+    let len = 0, tail = s.length - 1;
+
+    while(tail >= 0 && s[tail] == ' ') tail--;
+    while(tail >= 0 && s[tail] != ' ') {
+        len++;
+        tail--;
+    }
+
+    return len;
+}
+
+// 3. Just one Loop and slightly different method
+const lengthLastWord3 = s => {
+    let len = 0;
+
+    for(let i = s.length - 1; i >= 0; i--) {
+        if(s.charAt(i) !== ' ') len++;
+        else {
+            if(len > 0) return len;
+        }
+    }
+
+    return len;
+}
+
 
